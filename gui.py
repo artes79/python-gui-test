55371b609195c08fa734df73872a366ddbccfbcc
Terje Urnes <terje@urnes.net>
2022-07-18 01:16:44 +0200

    Adding button

--- gui.py
+++ gui.py
@@ -11,4 +11,7 @@ myLabel2 = Label(root, text="My name is secrete")
 myLabel1.grid(row=0, column=0)
 myLabel2.grid(row=1, column=1)
 
+myButton = Button(root, text="Click Me!")
+myButton.grid(row=0, column=1)
+
 root.mainloop()
