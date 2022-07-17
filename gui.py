132283766c019e35ba142b85a29a025787b15fac
Terje Urnes <terje@urnes.net>
2022-07-18 01:31:21 +0200

    Made button clickable

--- gui.py
+++ gui.py
@@ -3,7 +3,9 @@ from tkinter import *
 
 root = Tk()
 
-
+def myClick():
+    myLabelClick = Label(root, text="Look! I clicked a Button!!")
+    myLabelClick.grid(row=1, column=0)
 
 myLabel1 = Label(root, text="Hello World!")
 myLabel2 = Label(root, text="My name is secrete")
@@ -11,7 +13,7 @@ myLabel2 = Label(root, text="My name is secrete")
 myLabel1.grid(row=0, column=0)
 myLabel2.grid(row=1, column=1)
 
-myButton = Button(root, text="Click Me!", state=DISABLED)
+myButton = Button(root, text="Click Me!", command=myClick) #state=DISABLED
 myButton.grid(row=0, column=1)
 
 root.mainloop()
