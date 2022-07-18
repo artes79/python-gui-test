5274b6855fc6a159819bfc6c7d251d2e84bcdf52
Terje Urnes <terje@urnes.net>
2022-07-18 03:38:19 +0200

    Lagt til GUI classen samt view modulen

---
+++ Classes/View/GUI.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+class GUI:
+
+    def __init__(self):
+        self.tkRoot = Tk()
+
+    def draw(self):
+        self.tkRoot.mainloop()
---
+++ Classes/View/__init__.py
@@ -0,0 +1,1 @@
+from Classes.View.GUI import *
