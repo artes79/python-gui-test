946d555566460b8a3213baa1164f9241a4975823
Terje Urnes <terje@urnes.net>
2022-07-19 19:34:12 +0200

    Adding Herbivore and Predator classes

---
+++ Classes/Model/Animals/Herbivore.py
@@ -0,0 +1,7 @@
+from Classes.Model.Animals import *
+
+
+class Herbivore(Animal):
+
+    def __init__(self, name, pos, orientation):
+        Animal.__init__(self, name, pos, orientation)
---
+++ Classes/Model/Animals/Predator.py
@@ -0,0 +1,8 @@
+from Classes.Model.Animals import *
+
+
+class Predator(Animal):
+
+    def __init__(self, name, pos, orientation):
+        Animal.__init__(self, name, pos, orientation)
+
