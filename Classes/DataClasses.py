82245c21b8e4f048a853effdf9f98e14f84c4016
Terje Urnes <terje@urnes.net>
2022-07-19 19:58:01 +0200

    Updating Animal

--- Classes/DataClasses.py
+++ Classes/DataClasses.py
@@ -1,3 +1,10 @@
 from dataclasses import dataclass
 
 
+@dataclass
+class Orientation:
+    xCoord: int
+    yCoord: int
+    direction: int
+    speed: float
+
--- Classes/Model/Animals/Animal.py
+++ Classes/Model/Animals/Animal.py
@@ -1,5 +1,9 @@
+from Classes.Model.Animals import *
 
-class Animal(object):
 
-    def __init__(self, name):
+class Animal(Living):
+
+    def __init__(self, name, pos, orientation):
         self.name = name
+        self.pos = pos
+        self.orientation = orientation
