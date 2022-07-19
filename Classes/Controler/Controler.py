0db0cd01358603888b99e92852e2beae3d58bbe1
Terje Urnes <terje@urnes.net>
2022-07-19 19:55:01 +0200

    Adding time and calculate timestep

--- Classes/Controler/Controler.py
+++ Classes/Controler/Controler.py
@@ -1,14 +1,19 @@
 from Classes import *
+import time
 
 
 class Controler(object):
 
     def __init__(self, name):
+
+        self.timeLastUpdate = time.time()
+
         self.name = name
         self.model = Model("model")
 
         self.gui = GUI(self)
         self.gui.draw()
+
     def executeOneStep(self):
         newTime = time.time()
         diffTime = newTime - self.timeLastUpdate
