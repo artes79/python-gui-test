4c68a5e6aca37dfdc9e09b176281fc0ab692a9ed
Terje Urnes <terje@urnes.net>
2022-07-19 19:47:47 +0200

    Removing method in controller

--- Classes/Controler/Controler.py
+++ Classes/Controler/Controler.py
@@ -6,7 +6,3 @@ class Controler(object):
     def __init__(self, name):
         self.name = name
         self.model = Model("model")
-
-    def makeView(self):
-        self.gui = GUI()
-        self.gui.draw()
--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from Classes import *
 class Main:
     def main(self):
         controler = Controler("main")
-        controler.makeView()
+
 
 if __name__ == "__main__":
     mainProg = Main()
