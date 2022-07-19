66392ab509ec2ff655c37d588135eddd75a06b5c
Terje Urnes <terje@urnes.net>
2022-07-19 19:50:55 +0200

    Setting up gui

--- Classes/Controler/Controler.py
+++ Classes/Controler/Controler.py
@@ -6,3 +6,6 @@ class Controler(object):
     def __init__(self, name):
         self.name = name
         self.model = Model("model")
+
+        self.gui = GUI(self)
+        self.gui.draw()
--- Classes/View/GUI.py
+++ Classes/View/GUI.py
@@ -2,8 +2,14 @@ from tkinter import *
 
 class GUI:
 
-    def __init__(self):
+
+    def __init__(self, controler):
+        self.controler = controler
         self.tkRoot = Tk()
+        self.myCanvas = Canvas(self.tkRoot, bg="blue", height=240, width=320)
+        self.myCanvas.pack()
+        self.windowOpen = True
+        self.tkRoot.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     def draw(self):
         self.tkRoot.mainloop()
