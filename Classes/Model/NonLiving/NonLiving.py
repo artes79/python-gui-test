cd8df579e93693551ae61cedb47702ab33740b6a
Terje Urnes <terje@urnes.net>
2022-07-24 10:27:15 +0200

    Adding interface to NonLiving and update

--- Classes/Model/NonLiving/NonLiving.py
+++ Classes/Model/NonLiving/NonLiving.py
@@ -1,7 +1,9 @@
 from Classes.Model.BaseComponent import *
 
 
-class NonLiving(BaseComponent):
+class NonLiving(BaseComponent, IImmortal):
 
-    def __init__(self):
-        self.name = "NonLiving"
+    def __init__(self, positioning: Positioning,
+                 spatialProperties: SpatialProperties):
+        super().__init__(positioning, spatialProperties)
+
---
+++ Interface/IImmortal.py
@@ -0,0 +1,4 @@
+
+
+class IImmortal:
+    pass
