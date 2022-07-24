002bc396578c3d42fa9f52c490ac4ff62eb33bf9
Terje Urnes <terje@urnes.net>
2022-07-24 10:43:30 +0200

    Some changes to gui class

--- Classes/View/GUI.py
+++ Classes/View/GUI.py
@@ -1,37 +1,34 @@
 from tkinter import *
 import time
 from PIL import Image, ImageTk
+from Classes import *
+from Classes.Model import *
+from Interface import *
+from Enums import *
+
 
 class GUI:
-
 
     images = []
 
     def __init__(self, controler):
         self.controler = controler
         self.tkRoot = Tk()
-        self.myCanvas = Canvas(self.tkRoot, bg="blue", height=240, width=320)
+        self.myCanvas = Canvas(self.tkRoot, bg="green", height=480, width=640)
         self.myCanvas.pack()
         self.windowOpen = True
         self.tkRoot.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     def draw(self):
         self.positionImage()
-        self.myCanvas.create_image(0, 120, image=self.image, tag="image1")
         while self.windowOpen:
             self.controler.executeOneStep()
-            self.drawElements()
-            #self.myCanvas.delete("all")
-            coords = self.myCanvas.coords("image1")
-            self.myCanvas.move("image1", 0.1, 0)
+            self.drawEntities()
             self.tkRoot.update()
             time.sleep(0.01)
 
     def on_closing(self):
         self.windowOpen = False
-
-    def drawElements(self):
-        self.test = "test"
 
     def positionImage(self):
         pilImage = Image.open("images/rock.png")
