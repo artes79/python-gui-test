1f1f01ad4ccd1db69ca15a7601a6640fd60ed0aa
Terje Urnes <terje@urnes.net>
2022-07-24 10:34:23 +0200

    Adding entityes to the game

--- Classes/Model/Model.py
+++ Classes/Model/Model.py
@@ -1,11 +1,31 @@
 from Classes import *
 from Classes.Model.Living import *
+from Classes.Model import *
+from random import randint
 
 
 class Model(object):
 
-    def __init__(self, name):
-        self.name = name
+    entityTypes = {
+        "Rock": Rock
+    }
+
+    def __init__(self):
+        self.propagateEntities()
+
+    def propagateEntities(self):
+        for _ in range(10):
+            for typeStr in self.entityTypes:
+                t = self.entityTypes[typeStr]
+                p = Positioning()
+                p.x = float(randint(1,639))
+                p.y = float(randint(1,479))
+                p.orientation = -45
+                inst = t(p)
+                inst.toString()
+                t.addToEntitySet(inst)
+
+
 
     def run(self, timeSpan):
         self.test = "test"
