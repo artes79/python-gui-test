a0b60e867fbf18a9d278641609dcf14e1a0078d3
Terje Urnes <terje@urnes.net>
2022-07-27 20:19:40 +0200

    loading difrent types of gameobjects in the Model.

--- Classes/Model/Model.py
+++ Classes/Model/Model.py
@@ -7,23 +7,46 @@ from random import randint
 class Model(object):
 
     entityTypes = {
-        "Rock": Rock
+        "Rock": Rock,
+        "Gress": Gress
     }
 
-    def __init__(self):
-        self.propagateEntities()
+    def __init__(self, typeData: dict):
+        self.propagateEntities(typeData)
 
-    def propagateEntities(self):
-        for _ in range(10):
+    def propagateEntities(self, typeData: dict):
+        for tKey, tData in typeData.items():
+            #tData = typeData[tIndex]
+            print(tData)
+            t = eval(tKey)
+            for _ in range(tData["init_count"]):
+                argList: list = ["test"]
+                for property, value in tData["propertys"].items():
+                    pt = eval(property)
+                    print(pt)
+                    p = pt()
+                    print(getattr(p, "x"))
+                    for attrIndex in tData["propertys"][property]:
+                        setattr(p,
+                                tData["propertys"][property][attrIndex],
+                                float(randint(1,400)))
+                    argList.append(p)
+                print(argList)
+                inst = t(argList)
+
+        for _ in range(100):
             for typeStr in self.entityTypes:
                 t = self.entityTypes[typeStr]
                 p = Positioning()
                 p.x = float(randint(1,639))
                 p.y = float(randint(1,479))
                 p.orientation = -45
-                inst = t(p)
-                inst.toString()
-                t.addToEntitySet(inst)
+                print(setattr(p, "x", 200))
+                print(p.x)
+                #inst = t(p)
+                #inst.toString()
+                #t.addToEntitySet(inst)
+
 
 
 
