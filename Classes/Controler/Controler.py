92d33ee8d809adbb5b5f33ff86b26be4a838aead
Terje Urnes <terje@urnes.net>
2022-07-27 20:22:22 +0200

    Loading gameobject data

--- Classes/Controler/Controler.py
+++ Classes/Controler/Controler.py
@@ -1,4 +1,5 @@
 from Classes import *
+import json
 import time
 
 
@@ -6,12 +7,15 @@ class Controler(object):
 
     def __init__(self, name):
 
+        gameDataStr = open('gameData.json')
+        self.gameData = json.load(gameDataStr)
+
         self.timeLastUpdate = time.time()
 
         self.name = name
-        self.model = Model()
+        self.model = Model(self.gameData)
 
-        self.gui = GUI(self)
+        self.gui = GUI(self, self.gameData)
         self.gui.draw()
 
     def executeOneStep(self):
