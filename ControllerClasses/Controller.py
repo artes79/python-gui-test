e16145f61ee2e83c2df111fab2d2fd2a729af974
Terje Urnes <terje@urnes.net>
2022-10-21 21:56:01 +0200

    Adding Controller

---
+++ ControllerClasses/Controller.py
@@ -0,0 +1,7 @@
+from ControllerClasses.IController import IController
+
+
+class Controller(IController):
+
+    def startGame():
+        pass
---
+++ ControllerClasses/IController.py
@@ -0,0 +1,7 @@
+
+
+class IController:
+
+    @staticmethod
+    def startGame():
+        pass
