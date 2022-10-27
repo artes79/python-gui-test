d1f82112b4758047cd00a02757ab23410d0fc940
Terje Urnes <terje@urnes.net>
2022-10-27 20:31:15 +0200

    Adding a main method

---
+++ Main.py
@@ -0,0 +1,11 @@
+from ControllerClasses.Controller import Controller
+
+
+class Main:
+
+    @staticmethod
+    def startGame():
+        Controller.startGame()
+
+if __name__ == "__main__":
+    Main.startGame()
