e7778789d6eba530f620cd8275cb029261511208
Terje Urnes <terje@urnes.net>
2022-10-27 20:40:42 +0200

    init and Create GUI

--- ViewClasses/View.py
+++ ViewClasses/View.py
@@ -5,3 +5,13 @@ class View(IView):
 
     def p(self):
         pass
+
+    @staticmethod
+    def initGUI():
+        View.rootGUI = gui.Tk()
+        View.rootGUI.protocol("WM_DELETE_WINDOW", View.onClosing)
+        View.mainView = gui.Frame(View.rootGUI)
+        View.mainView.pack(fill=gui.BOTH, expand=gui.YES)
+        View.gameBoardLayer = gui.Canvas(View.mainView, width=640, height=480, bg="#f5deb3", highlightthickness=0)
+        View.gameBoardLayer.pack(fill=gui.BOTH, expand=gui.YES)
+        View.gameRunning = True
