83dbb958680582670cae33794b14aa5c6af478cd
Terje Urnes <terje@urnes.net>
2022-10-27 02:56:05 +0200

    Adding GameBoard, implementing IGameBoard.

---
+++ ViewClasses/GameBoard.py
@@ -0,0 +1,18 @@
+import tkinter as gui
+from ViewClasses.IGameBoard import IGameBoard
+
+
+class GameBoard(IGameBoard):
+
+    @staticmethod
+    def initBoard(playLayers: gui.Canvas):
+        pass
+
+    @staticmethod
+    def run():
+        pass
+
+    # tags: all: game-item, grups: ground, on-ground, above-ground
+    @staticmethod
+    def arangeGameItems():
+        pass
