44f3659cf05554dad474be56440b6c6a4c648421
Terje Urnes <terje@urnes.net>
2022-10-27 02:39:00 +0200

    Adding methods to IView

--- ViewClasses/IView.py
+++ ViewClasses/IView.py
@@ -2,5 +2,24 @@
 
 class IView:
 
-    def p(self):
+    @staticmethod
+    def startGameView() -> None:
+        pass
+
+    # <gui.Tk()>.protocol("WM_DELETE_WINDOW", View.onClosing)
+    @staticmethod
+    def initGUI() -> None:
+        pass
+
+    @staticmethod
+    def runGUI() -> None:
+        pass
+
+    @staticmethod
+    def arangeLayers() -> None:
+        pass
+
+    # Stop View.runGUI() når vinduet lukkes
+    @staticmethod
+    def onClosing() -> None:
         pass
