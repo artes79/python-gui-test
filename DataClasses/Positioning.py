ea46973211dac11f1adddb99987cce6a1a34faca
Terje Urnes <terje@urnes.net>
2022-11-10 20:51:57 +0100

    Adding properties and variables

--- DataClasses/Positioning.py
+++ DataClasses/Positioning.py
@@ -1,5 +1,55 @@
+import math
+import random
+from dataclasses import dataclass
+import numpy as np
 from DataClasses.IPositioning import IPositioning
+from ModelClasses.IEntity import IEntity
 
 
+#@dataclass
 class Positioning(IPositioning):
-    pass
+
+    _coord: np.ndarray
+    _orientation: float = 0
+
+    _movable: bool
+
+    _boundingBoxWidth: int
+    _boundingBoxHeight: int
+
+    _worldWidth: int = 0
+    _worldHeight: int = 0
+    _lastWidthDiff: int = 0
+    _lastHeightDiff: int = 0
+
+    @property
+    def coord(self) -> np.ndarray:
+        return self._coord
+
+    @property
+    def x(self) -> float:
+        return self._coord[0]
+
+    @property
+    def centerX(self) -> float:
+        return self._coord[0]
+
+    @property
+    def xAsInt(self) -> int:
+        return np.round(self._coord[0])
+
+    @property
+    def y(self) -> float:
+        return self._coord[1]
+
+    @property
+    def centerY(self) -> float:
+        return self._coord[1]
+
+    @property
+    def yAsInt(self) -> float:
+        return np.round(self._coord[1])
+
+    @property
+    def orientation(self) -> float:
+        return self._orientation
