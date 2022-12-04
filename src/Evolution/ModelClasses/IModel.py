8e7e0185d9433999fc230c5db595f1a1dd89e4e9
Terje Urnes <terje@urnes.net>
2022-12-04 09:50:27 +0100

    Implements model class

--- src/Evolution/ModelClasses/IModel.py
+++ src/Evolution/ModelClasses/IModel.py
@@ -1,4 +1,16 @@
+from Evolution.ModelClasses.IEntity import IEntity
 
 
 class IModel:
-    pass
+
+    @staticmethod
+    def StartGameModel(width: int, height: int) -> None:
+        pass
+
+    @staticmethod
+    def RunOneStep(duration: float) -> None:
+        pass
+
+    @staticmethod
+    def GetEntities() -> list[type[IEntity]]:
+        pass
--- src/Evolution/ModelClasses/Model.py
+++ src/Evolution/ModelClasses/Model.py
@@ -1,4 +1,21 @@
+import numpy as np
 from Evolution.ModelClasses.IModel import IModel
+from Evolution.DataClasses.WorldData import WorldData
+from Evolution.DataClasses.Positioning import Positioning
+from Evolution.ModelClasses.IEntity import IEntity
 
 class Model(IModel):
-    pass
+
+    @staticmethod
+    def StartGameModel(width: int, height: int) -> None:
+        worldData = WorldData()
+        worldData.size = np.array([width, height], dtype=float)
+        Positioning.SetWorld(worldData)
+
+    @staticmethod
+    def RunOneStep(duration: float) -> None:
+        pass
+
+    @staticmethod
+    def GetEntities() -> list[type[IEntity]]:
+        pass
