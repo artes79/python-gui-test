7c8aed974f416aa3601d88bc81aa442375a7220d
Terje Urnes <terje@urnes.net>
2022-12-15 19:18:57 +0100

    Removing SetRandomPosition

--- src/Evolution/DataClasses/GroundPositioning.py
+++ src/Evolution/DataClasses/GroundPositioning.py
@@ -1,12 +1,10 @@
-from random import random
+import time
+from random import random, seed
 import numpy as np
 from Evolution.DataClasses.Positioning import Positioning
 from Evolution.DataClasses.IGroundPositioning import IGroundPositioning
+from Evolution.ModelClasses.BaseEntity import BaseEntity
+from Evolution.ModelClasses.IEntity import IEntity
 
 
 class GroundPositioning(Positioning, IGroundPositioning):
-
-    def SetRandomPosition(self) -> None:
-        x = np.round(random() * ((Positioning._world.width - self._physical.extent[0] * 2) / (self._physical.extent[0] * 2))) * (self._physical.extent[0] * 2) + self._physical.extent[0]
-        y = np.round(random() * ((Positioning._world.height - self._physical.extent[1] * 2) / (self._physical.extent[1] * 2))) * (self._physical.extent[1] * 2) + self._physical.extent[1]
-        self.position = np.array([x, y], dtype=float)
