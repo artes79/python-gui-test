7939ea733aa28f0c1dfee4fa3a55eb7b3fc598e8
Terje Urnes <terje@urnes.net>
2022-12-15 19:20:19 +0100

    adding ground tabel and methods.

--- src/Evolution/ModelClasses/BaseEntity.py
+++ src/Evolution/ModelClasses/BaseEntity.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from PIL.ImageTk import PhotoImage
+import numpy as np
 
 from Evolution.ModelClasses.IBaseEntity import IBaseEntity
 from Evolution.ModelClasses.IEntity import IEntity
@@ -8,6 +9,7 @@ class BaseEntity(IBaseEntity):
 
     _entitiesCensus: list[type[IEntity]] = []
     _lastGeneratdId: int = 0
+    _groundTable: list = [[None] * 32 for i in range(44)]
 
     @staticmethod
     def GenerateId() -> str:
@@ -26,3 +28,19 @@ class BaseEntity(IBaseEntity):
     @staticmethod
     def AddEntity(entity: IEntity) -> None:
         BaseEntity._entitiesCensus.append(entity)
+
+    @staticmethod
+    def AddEntityToGround(entity: IEntity) -> None:
+        x = int(round(entity.position.position[0] / 15)) - 1
+        y = int(round(entity.position.position[1] / 15)) - 1
+        BaseEntity._groundTable[x][y] = entity
+
+    @staticmethod
+    def GetEntityFromGroundSquare(px: int, py: int) -> IEntity:
+        x = int(round(px / 15)) - 1
+        y = int(round(py / 15)) - 1
+        return BaseEntity._groundTable[x][y]
+
+    @staticmethod
+    def IsGroundSquareOcupide(x: int, y: int) -> bool:
+        return BaseEntity.GetEntityFromGroundSquare(x, y) != None
